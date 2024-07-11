42956adebca0f072a9d29ce1e5c93f2c351cb2f4
Malanca Daniel <malanca.daniel23@gmail.com>
2024-07-11 10:35:29 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,172 @@
+mod characters;
+mod virtual_machine;
+
+use sdl2::event::Event;
+use sdl2::keyboard::{Keycode, Scancode};
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use std::sync::atomic::Ordering;
+use std::{
+    path::PathBuf,
+    time::{Duration, Instant},
+};
+use virtual_machine::{CanvasColor, VirtualMachine};
+
+use sdl2::audio::{AudioCallback, AudioSpecDesired};
+
+const PIXEL_SIZE: usize = 12;
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+const WINDOW_Y: u32 = (PIXEL_SIZE * HEIGHT) as u32;
+const WINDOW_X: u32 = (PIXEL_SIZE * WIDTH) as u32;
+const REFRESH_RATE: u32 = 60;
+const FRAME_TIME: Duration = Duration::from_nanos(1_000_000_000 / REFRESH_RATE as u64);
+const CLOCK_HZ: u32 = 10000;
+
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32,
+}
+
+impl AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        for x in out.iter_mut() {
+            *x = if self.phase <= 0.5 {
+                self.volume
+            } else {
+                -self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
+        }
+    }
+}
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let audio_subsystem = sdl_context.audio()?;
+
+    let window = video_subsystem
+        .window("Chip 8", WINDOW_X, WINDOW_Y)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44_100),
+        channels: Some(1),
+        samples: None,
+    };
+
+    let device = audio_subsystem.open_playback(None, &desired_spec, |spec| SquareWave {
+        phase_inc: 440.0 / spec.freq as f32,
+        phase: 0.0,
+        volume: 0.25,
+    })?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let path = PathBuf::from(std::env::args().nth(1).unwrap());
+
+    let (mut machine, machine_canvas_mutex, current_pressed_key, sound_timer) =
+        VirtualMachine::new(&path);
+
+    std::thread::spawn({
+        move || loop {
+            machine.execute_opcode();
+            std::thread::sleep(Duration::from_secs_f64(1.0 / CLOCK_HZ as f64));
+        }
+    });
+
+    'main: loop {
+        let now = Instant::now();
+
+        if sound_timer.load(Ordering::Relaxed) > 0 {
+            device.resume();
+        } else {
+            device.pause();
+        }
+
+        canvas.set_draw_color(Color::WHITE);
+        canvas.clear();
+
+        let machine_canvas = machine_canvas_mutex.lock().unwrap();
+        let mut rects = Vec::with_capacity(WIDTH * HEIGHT);
+
+        canvas.set_draw_color(Color::BLACK);
+        for x in 0..WIDTH {
+            for y in 0..HEIGHT {
+                if machine_canvas[y][x] == CanvasColor::Black {
+                    let rect = Rect::new(
+                        (PIXEL_SIZE * x) as i32,
+                        (PIXEL_SIZE * y) as i32,
+                        PIXEL_SIZE as u32,
+                        PIXEL_SIZE as u32,
+                    );
+                    rects.push(rect);
+                }
+            }
+        }
+
+        drop(machine_canvas);
+
+        canvas.fill_rects(&rects)?;
+        canvas.present();
+
+        while now.elapsed() < FRAME_TIME {
+            if let Some(event) = event_pump.wait_event_timeout(now.elapsed().as_millis() as u32) {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'main,
+                    Event::KeyDown {
+                        scancode, repeat, ..
+                    } => {
+                        if repeat {
+                            continue;
+                        }
+                        let mut mutex = current_pressed_key.lock().unwrap();
+                        *mutex = code_to_code(scancode);
+                    }
+                    Event::KeyUp { .. } => {
+                        let mut mutex = current_pressed_key.lock().unwrap();
+                        *mutex = None;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn code_to_code(scancode: Option<Scancode>) -> Option<u8> {
+    scancode.and_then(|code| match code {
+        Scancode::Num1 => Some(0x1),
+        Scancode::Num2 => Some(0x2),
+        Scancode::Num3 => Some(0x3),
+        Scancode::Num4 => Some(0xC),
+        Scancode::Q => Some(0x4),
+        Scancode::W => Some(0x5),
+        Scancode::E => Some(0x6),
+        Scancode::R => Some(0xD),
+        Scancode::A => Some(0x7),
+        Scancode::S => Some(0x8),
+        Scancode::D => Some(0x9),
+        Scancode::F => Some(0xE),
+        Scancode::Z => Some(0xA),
+        Scancode::X => Some(0x0),
+        Scancode::C => Some(0xB),
+        Scancode::V => Some(0xF),
+        _ => None,
+    })
+}
---
+++ src/virtual_machine.rs
@@ -0,0 +1,374 @@
+use std::{
+    path::Path,
+    sync::{
+        atomic::{AtomicU8, Ordering},
+        Arc, Mutex,
+    },
+    time::Duration,
+};
+
+use crate::{characters, HEIGHT, WIDTH};
+
+enum Relation {
+    Equal,
+    NotEqual,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum CanvasColor {
+    White,
+    Black,
+}
+
+impl CanvasColor {
+    fn change(&mut self) -> bool {
+        match self {
+            CanvasColor::White => {
+                *self = CanvasColor::Black;
+                false
+            }
+            CanvasColor::Black => {
+                *self = CanvasColor::White;
+                true
+            }
+        }
+    }
+}
+
+pub struct VirtualMachine {
+    memory: [u8; 0x1000],
+    registers: [u8; 16],
+    stack: [u16; 100],
+    stack_size: u8,
+    i: u16,
+    pc: u16,
+    delay_timer: Arc<AtomicU8>,
+    sound_timer: Arc<AtomicU8>,
+    current_pressed_key: Arc<Mutex<Option<u8>>>,
+    should_increment_pc: bool,
+    canvas: Arc<Mutex<[[CanvasColor; WIDTH]; HEIGHT]>>,
+}
+
+impl VirtualMachine {
+    pub fn new(
+        path: &Path,
+    ) -> (
+        Self,
+        Arc<Mutex<[[CanvasColor; WIDTH]; HEIGHT]>>,
+        Arc<Mutex<Option<u8>>>,
+        Arc<AtomicU8>,
+    ) {
+        let rom = std::fs::read(path).unwrap();
+        let mut machine = Self {
+            memory: [0; 0x1000],
+            registers: [0; 16],
+            stack: [0; 100],
+            stack_size: 0,
+            i: 0x200,
+            pc: 0x200,
+            delay_timer: Arc::new(AtomicU8::new(0)),
+            sound_timer: Arc::new(AtomicU8::new(0)),
+            current_pressed_key: Arc::new(Mutex::new(None)),
+            should_increment_pc: false,
+            canvas: Arc::new(Mutex::new([[CanvasColor::White; WIDTH]; HEIGHT])),
+        };
+
+        machine.memory[0x200..(0x200 + rom.len())].copy_from_slice(&rom);
+        machine.memory[0x50..0xA0].copy_from_slice(&characters::CHARS);
+
+        std::thread::spawn({
+            let sound_timer = machine.sound_timer.clone();
+            let delay_timer = machine.delay_timer.clone();
+            move || loop {
+                let sound_value = sound_timer.load(Ordering::Relaxed);
+                if sound_value > 0 {
+                    sound_timer.store(sound_value - 1, Ordering::Relaxed);
+                }
+
+                let delay_value = delay_timer.load(Ordering::Relaxed);
+                if delay_value > 0 {
+                    delay_timer.store(delay_value - 1, Ordering::Relaxed);
+                }
+
+                std::thread::sleep(Duration::from_secs_f64(1.0 / 60.0));
+            }
+        });
+
+        let canvas_arc = machine.canvas.clone();
+        let key_arc = machine.current_pressed_key.clone();
+        let sound_arc = machine.sound_timer.clone();
+        (machine, canvas_arc, key_arc, sound_arc)
+    }
+
+    fn get_memory(&self, address: u16) -> u8 {
+        self.memory[address as usize]
+    }
+
+    fn set_memory(&mut self, address: u16, byte: u8) {
+        self.memory[address as usize] = byte
+    }
+
+    fn get_register(&self, register: u8) -> u8 {
+        self.registers[register as usize]
+    }
+
+    fn set_register(&mut self, register: u8, byte: u8) {
+        self.registers[register as usize] = byte;
+    }
+
+    fn set_flag(&mut self, flag: u8) {
+        self.registers[15] = flag;
+    }
+
+    fn call(&mut self, address: u16) {
+        self.inc_pc();
+        self.stack[self.stack_size as usize] = self.pc;
+        self.stack_size += 1;
+        self.pc = address;
+        self.should_increment_pc = false;
+    }
+
+    fn _return(&mut self) {
+        assert!(self.stack_size >= 1);
+        self.stack_size -= 1;
+        self.pc = self.stack[self.stack_size as usize];
+        self.should_increment_pc = false;
+    }
+
+    fn jump_to(&mut self, address: u16) {
+        self.pc = address;
+        self.should_increment_pc = false;
+    }
+
+    fn inc_pc(&mut self) {
+        self.pc += 2;
+    }
+
+    fn skip_if_byte(&mut self, register: u8, byte: u8, relation: Relation) {
+        let value = self.get_register(register);
+        let condition = match relation {
+            Relation::Equal => value == byte,
+            Relation::NotEqual => value != byte,
+        };
+
+        if condition {
+            self.inc_pc();
+        }
+    }
+
+    fn skip_if_register(&mut self, register1: u8, register2: u8, relation: Relation) {
+        let value1 = self.get_register(register1);
+        let value2 = self.get_register(register2);
+        let condition = match relation {
+            Relation::Equal => value1 == value2,
+            Relation::NotEqual => value1 != value2,
+        };
+
+        if condition {
+            self.inc_pc();
+        }
+    }
+
+    fn skip_if_key(&mut self, register: u8, relation: Relation) {
+        let value = self.get_register(register);
+
+        let mutex = self.current_pressed_key.lock().unwrap();
+        let key = *mutex;
+        drop(mutex);
+
+        let condition = match relation {
+            Relation::Equal => key.is_some_and(|key| key == value),
+            Relation::NotEqual => key.is_none() || key.is_some_and(|key| key != value),
+        };
+
+        if condition {
+            self.inc_pc();
+        }
+    }
+
+    fn add_byte(&mut self, register: u8, byte: u8) {
+        let value = self.get_register(register);
+        self.set_register(register, value.wrapping_add(byte));
+    }
+
+    pub fn execute_opcode(&mut self) {
+        self.should_increment_pc = true;
+        let (byte1, byte2) = (self.get_memory(self.pc), self.get_memory(self.pc + 1));
+        // println!("Byte1: {:02X}, Byte2: {:02X}", byte1, byte2);
+        let address = ((byte1 as u16 & 0x0F) << 8) | (byte2 as u16);
+        let register_x = byte1 & 0x0F;
+        let register_y = byte2 >> 4;
+        let last_nibble = byte2 & 0x0F;
+
+        match (byte1 & 0xF0) >> 4 {
+            0x0 => match byte2 {
+                0xE0 => self.clear_canvas(),
+                0xEE => self._return(),
+                _ => self.call(address),
+            },
+            0x1 => self.jump_to(address),
+            0x2 => self.call(address),
+            0x3 => self.skip_if_byte(register_x, byte2, Relation::Equal),
+            0x4 => self.skip_if_byte(register_x, byte2, Relation::NotEqual),
+            0x5 => {
+                assert_eq!(last_nibble, 0);
+                self.skip_if_register(register_x, register_y, Relation::Equal);
+            }
+            0x6 => self.set_register(register_x, byte2),
+            0x7 => self.add_byte(register_x, byte2),
+            0x8 => self.execute_math(last_nibble, register_x, register_y),
+            0x9 => {
+                assert_eq!(last_nibble, 0);
+                self.skip_if_register(register_x, register_y, Relation::NotEqual);
+            }
+            0xA => self.i = address,
+            0xB => {
+                let new_pc = self.get_register(0) as u16 + address;
+                self.pc = new_pc;
+                self.should_increment_pc = false;
+            }
+            0xC => {
+                let random_num = fastrand::u8(..) & byte2;
+                self.set_register(register_x, random_num);
+            }
+            0xD => {
+                let x = self.get_register(register_x);
+                let y = self.get_register(register_y);
+                let height = last_nibble;
+
+                self.draw(x, y, height);
+            }
+            0xE => match byte2 {
+                0x9E => self.skip_if_key(register_x, Relation::Equal),
+                0xA1 => self.skip_if_key(register_x, Relation::NotEqual),
+                _ => unreachable!(),
+            },
+            0xF => match byte2 {
+                0x07 => {
+                    let value = self.delay_timer.load(Ordering::Relaxed);
+                    self.set_register(register_x, value);
+                }
+                0x0A => {
+                    let mut mutex = self.current_pressed_key.lock().unwrap();
+                    let value = *mutex;
+                    *mutex = None;
+                    drop(mutex);
+
+                    if let Some(code) = value {
+                        self.set_register(register_x, code);
+                    } else {
+                        return;
+                    }
+                }
+                0x15 => {
+                    let value = self.get_register(register_x);
+                    self.delay_timer.store(value, Ordering::Relaxed);
+                }
+                0x18 => {
+                    let value = self.get_register(register_x);
+                    self.sound_timer.store(value, Ordering::Relaxed);
+                }
+                0x1E => self.i += self.get_register(register_x) as u16,
+                0x29 => self.i = 0x50 + self.get_register(register_x) as u16 * 5,
+                0x33 => self.set_bcd(register_x),
+                0x55 => self.dump_registers(register_x),
+                0x65 => self.load_registers(register_x),
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
+        }
+
+        if self.should_increment_pc {
+            self.inc_pc()
+        }
+    }
+
+    fn dump_registers(&mut self, register: u8) {
+        for index in 0u8..=register {
+            self.set_memory(self.i + index as u16, self.get_register(index));
+        }
+    }
+
+    fn load_registers(&mut self, register: u8) {
+        for index in 0u8..=register {
+            self.set_register(index, self.get_memory(self.i + index as u16));
+        }
+    }
+
+    fn set_bcd(&mut self, register: u8) {
+        let mut value = self.get_register(register);
+        let units = value % 10;
+        value /= 10;
+        let tens = value % 10;
+        value /= 10;
+        let hundreds = value;
+
+        self.set_memory(self.i, hundreds);
+        self.set_memory(self.i + 1, tens);
+        self.set_memory(self.i + 2, units);
+    }
+
+    fn execute_math(&mut self, operation: u8, register_x: u8, register_y: u8) {
+        let value_x = self.get_register(register_x);
+        let value_y = self.get_register(register_y);
+
+        let result = match operation {
+            0x0 => value_y,
+            0x1 => value_x | value_y,
+            0x2 => value_x & value_y,
+            0x3 => value_x ^ value_y,
+            0x4 => {
+                let (result, flag) = value_x.overflowing_add(value_y);
+                self.set_flag(flag as u8);
+                result
+            }
+            0x5 => {
+                let (result, flag) = value_x.overflowing_sub(value_y);
+                self.set_flag(!flag as u8);
+                result
+            }
+            0x6 => {
+                self.set_flag(value_x & 1);
+                value_x >> 1
+            }
+            0x7 => {
+                let (result, flag) = value_y.overflowing_sub(value_x);
+                self.set_flag(!flag as u8);
+                result
+            }
+            0xE => {
+                self.set_flag(value_x >> 7);
+                value_x << 1
+            }
+            _ => panic!(),
+        };
+
+        self.set_register(register_x, result);
+    }
+
+    pub fn clear_canvas(&mut self) {
+        let mut canvas = self.canvas.lock().unwrap();
+        canvas
+            .iter_mut()
+            .for_each(|row| row.fill(CanvasColor::White));
+    }
+
+    fn draw(&mut self, x: u8, y: u8, height: u8) {
+        let mut canvas = self.canvas.lock().unwrap();
+        let mut collision = false;
+        for dy in 0..height {
+            let byte = self.get_memory(self.i + dy as u16);
+            for dx in 0..8 {
+                let pixel = byte & (0x80 >> dx) != 0;
+                if pixel {
+                    collision |=
+                        canvas[(y + dy) as usize % HEIGHT][(x + dx) as usize % WIDTH].change();
+                }
+            }
+        }
+
+        drop(canvas);
+
+        self.set_flag(collision as u8);
+    }
+}
